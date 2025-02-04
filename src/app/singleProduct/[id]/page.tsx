d8051e4d91678b2a07c19f2eedc5013905f564ef
Kashaf Naz <kashafnazk389@gmail.com>
2025-02-04 20:10:10 +0500

    hackaton-3 next.js

--- src/app/singleProduct/[id]/page.tsx
+++ src/app/singleProduct/[id]/page.tsx
@@ -2,7 +2,7 @@ import { client } from "@/sanity/lib/client";
 import ProductDetailClient from "@/app/component/productDetail";
 
 async function getProduct(id: string) {
-  return await client.fetch(`*[_type == "products" && _id == $id][0]`, { id });
+  return client.fetch(`*[_type == "products" && _id == $id][0]`, { id });
 }
 
 interface ProductDetailProps {
@@ -12,9 +12,5 @@ interface ProductDetailProps {
 export default async function ProductDetail({ params }: ProductDetailProps) {  
   const product = await getProduct(params.id);
 
-  if (!product) {
-    return <p className="text-center text-red-500">Product not found!</p>;
-  }
-
-  return <ProductDetailClient product={product} />;
+  return product ? <ProductDetailClient product={product} /> : <p className="text-center text-red-500">Product not found!</p>;
 }
