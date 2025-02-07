a7b4035c187ab6563ab13d192b8e384aee856cef
Jaweria <168368019+Jiyakhan321@users.noreply.github.com>
2025-02-07 17:30:03 +0500

    hackathon-3 next.js

--- src/app/singleProduct/[id]/page.tsx
+++ src/app/singleProduct/[id]/page.tsx
@@ -1,7 +1,15 @@
 import { client } from "@/sanity/lib/client";
 import ProductDetailClient from "@/app/component/productDetail";
 
-const ProductDetailPage = async ({ params }: { params: { id: string } }) => {
+
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
+
+const ProductDetailPage = async ({ params }: PageProps) => {
+  
   async function getProduct(id: string) {
     try {
       return await client.fetch(`*[_type == "products" && _id == $id][0]`, { id });
@@ -12,12 +20,15 @@ const ProductDetailPage = async ({ params }: { params: { id: string } }) => {
   }
 
   try {
+    
     const product = await getProduct(params.id);
 
+    
     if (!product) {
       return <p className="text-center text-red-500">Product not found!</p>;
     }
 
+    
     return <ProductDetailClient product={product} />;
   } catch (error) {
     console.error("Error rendering product detail page:", error);
@@ -26,4 +37,3 @@ const ProductDetailPage = async ({ params }: { params: { id: string } }) => {
 };
 
 export default ProductDetailPage;
-
